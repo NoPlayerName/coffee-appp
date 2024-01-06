19583507ae375668f7d716cf6265c38498a76d32
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2024-01-06 20:03:41 +0700

    add first content

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,9 +1,11 @@
 @extends('layout.main')
 
-@section('contrainer')
-    <div>
-        <p>
-            Hellow Wolrd
-        </p>
-    </div>
+@section('container')
+    <section class="hero" id="home">
+        <main class="content">
+            <h1>Mari Nikmati Secangkir <span>Kopi</span></h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, illo!</p>
+            <a href="#" class="cta">Beli Sekarang</a>
+        </main>
+    </section>
 @endsection
