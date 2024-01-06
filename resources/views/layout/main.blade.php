4885bc5034cb44f1fcd1df4907e822ce88ba0371
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2024-01-06 18:02:38 +0700

    add main layout

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,36 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Kopi Kenangan Senja</title>
+
+    {{-- FONTS --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,700;1,700&display=swap"
+        rel="stylesheet">
+
+    {{-- Icons --}}
+    <script src="https://unpkg.com/feather-icons"></script>
+
+    {{-- CSS --}}
+    <link rel="stylesheet" href="{{ url('assets/css/style.css') }}">
+</head>
+
+<body>
+    {{-- Navbar Start --}}
+    <x-navbar />
+    {{-- Navbar End --}}
+    <div class="container">
+        @yield('container')
+    </div>
+    {{-- feather icons --}}
+    <script>
+        feather.replace();
+    </script>
+    <script src="{{ url('assets/js/script.js') }}"></script>
+</body>
+
+</html>
