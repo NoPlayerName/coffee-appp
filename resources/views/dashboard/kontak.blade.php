ed02f701d64b215cfc8d2655c000143452336f40
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2024-01-24 17:44:53 +0700

    add kontak

---
+++ resources/views/dashboard/kontak.blade.php
@@ -0,0 +1,32 @@
+<section id="contact" class="contact">
+    <h2><span>Kontak</span> Kami</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, odit!</p>
+
+    <div class="row">
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3057.6349271497443!2d107.35393789358646!3d-6.360221610276137!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69769e704ab6d9%3A0xf30eade0b261ce1e!2sSasak%20Misran!5e0!3m2!1sid!2sid!4v1706083674288!5m2!1sid!2sid"
+            allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="maps"></iframe>
+        <form action="">
+
+            <div class="input-group">
+                <i data-feather="user"></i>
+                <input type="text" placeholder="nama">
+            </div>
+
+            <div class="input-group">
+                <i data-feather="mail"></i>
+                <input type="email" name="mail" id="" placeholder="E-mail">
+            </div>
+            <div class="input-group">
+                <i data-feather="phone"></i>
+                <input type="text" placeholder="no hp">
+            </div>
+
+            <button type="submit" class="btn">Kirim Pesan</button>
+
+        </form>
+
+    </div>
+
+
+</section>
