2e376fe91890a3167fed77a0bf158c9dd58b7e6b
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2024-01-24 17:44:47 +0700

    add footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,18 @@
+<footer>
+    <div class="social">
+        <a href="#"><i data-feather="instagram"></i></a>
+        <a href="#"><i data-feather="twitter"></i></a>
+        <a href="#"><i data-feather="facebook"></i></a>
+    </div>
+
+    <div class="links">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#menu">Menu</a>
+        <a href="#contact">Kontak</a>
+    </div>
+
+    <div class="credit">
+        <p>Created by <a href="">Muhammad Ruhiyat</a>. | &copy; 2024 - {{ date('Y') }}.</p>
+    </div>
+</footer>
