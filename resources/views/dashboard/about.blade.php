2c2f6d4d8a121652afebd5a6697e06dc1958710d
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2024-01-30 15:48:10 +0700

    change lorem

--- resources/views/dashboard/about.blade.php
+++ resources/views/dashboard/about.blade.php
@@ -10,9 +10,8 @@
             <h3>Kenapa memilih kopi kami?</h3>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam fuga nesciunt repellat odio? Cumque, atque?
             </p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium sint, nobis vel, inventore veniam
-                consequatur assumenda distinctio voluptatem fugit in blanditiis optio deserunt. Natus optio, voluptates
-                ex nesciunt modi expedita.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit vero facilis cum, nihil voluptatibus
+                eos aliquam blanditiis perspiciatis dolores placeat!</p>
 
         </div>
     </div>
